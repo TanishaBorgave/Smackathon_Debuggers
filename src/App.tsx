83f2f4d816517c17a2b39aac0ce116f53fb1eff1
TanishaBorgave <tanishaborgave1@gmail.com>
2025-08-23 11:42:09 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
-import AIDoctorChat from "./components/AIDoctorChat";
 
 // Pages
 import Home from "./pages/Home";
@@ -37,7 +36,6 @@ const App = () => (
                   <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
-          <AIDoctorChat />
         </div>
       </BrowserRouter>
     </TooltipProvider>
