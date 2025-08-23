bb2ec2ef554676c6b872dc7b51316d39a92a18d6
TanishaBorgave <tanishaborgave1@gmail.com>
2025-08-23 11:37:44 +0530

    Add AIDoctorChat component to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import AIDoctorChat from "./components/AIDoctorChat";
 
 // Pages
 import Home from "./pages/Home";
@@ -36,6 +37,7 @@ const App = () => (
                   <Route path="*" element={<NotFound />} />
             </Routes>
           </main>
+          <AIDoctorChat />
         </div>
       </BrowserRouter>
     </TooltipProvider>
